f96d2090d5e543ea7ed47ac8ac7e6f3ac9df475c
start.vaadin.com <no-reply@vaadin.com>
2023-10-26 06:02:26 +0000

    Generated project

---
+++ src/main/java/org/vaadin/wajtr/views/helloworld/HelloworldView.java
@@ -0,0 +1,33 @@
+package org.vaadin.wajtr.views.helloworld;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.vaadin.wajtr.views.MainLayout;
+
+@PageTitle("Hello world")
+@Route(value = "", layout = MainLayout.class)
+public class HelloworldView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloworldView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
