673dab112fb2cf7d0be07cdf6f2c0b70e7161a1d
Bretislav Wajtr <bretislav@vaadin.com>
2023-10-26 08:15:49 +0200

    Added example of the all rows visible grid

--- src/main/java/org/vaadin/wajtr/views/helloworld/HelloworldView.java
+++ src/main/java/org/vaadin/wajtr/views/helloworld/HelloworldView.java
@@ -1,33 +1,43 @@
 package org.vaadin.wajtr.views.helloworld;
 
-import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.vaadin.wajtr.views.MainLayout;
 
+import java.util.UUID;
+
 @PageTitle("Hello world")
 @Route(value = "", layout = MainLayout.class)
-public class HelloworldView extends HorizontalLayout {
-
-    private TextField name;
-    private Button sayHello;
+public class HelloworldView extends VerticalLayout {
 
     public HelloworldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
+        Div div = new Div();
+        div.setHeight("300px");
+        add(div);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        var grid = new Grid<>(Person.class, false);
+        grid.setItems();
+        grid.addColumn(Person::name)
+                .setKey(UUID.randomUUID().toString()).setHeader("First name")
+                .setSortable(true);
 
-        add(name, sayHello);
+        grid.setItems(new Person("John"), new Person("Mary"), new Person("Joe"));
+        grid.setAllRowsVisible(true);
+        add(grid);
+
+        Div divBelow = new Div();
+        divBelow.setHeight("2000px");
+        add(divBelow);
     }
-
 }
+
+record Person(String name) {
+}
+
+
